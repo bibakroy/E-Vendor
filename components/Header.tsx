a0c44437ea8a4c58423b3541bbeaa56a7f03ccd1
bibakroy <bibak68@student.sust.edu>
2022-04-12 02:05:47 +0600

    header section modified: added responsiveness

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,40 +4,46 @@ import {
   MenuIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import { useRouter } from "next/router";
 
 export const Header = () => {
+  const router = useRouter();
   return (
-    <header>
+    <header className="sticky top-0 z-50">
       {/* top header */}
-      <div className="bg-self_blue flex items-center flex-grow p-1 py-2">
-        <div className="flex items-center flex-grow sm:flex-grow-0">
+      <div className="bg-self_blue flex items-center flex-grow p-1 py-3">
+        <div className="flex items-center flex-grow md:flex-grow-0 flex-shrink-0 mr-3 ml-4">
           <Image
             src="/logo.svg"
             width={100}
             height={30}
-            objectFit="contain"
+            // objectFit="contain"
             className="cursor-pointer"
+            onClick={() => router.push("/")}
           />
         </div>
 
-        <div className="bg-yellow-400 hover:bg-yellow-500 flex items-center h-10 rounded-md flex-grow cursor-pointer">
+        <div className="bg-yellow-400 hover:bg-yellow-500 hidden md:flex items-center h-10 rounded-md flex-grow flex-shrink cursor-pointer">
           <input
             type="text"
-            className="h-full  p-2 flex-grow flex-shrink rounded-l-md focus:outline-none px-3"
+            className="h-full p-2 flex-grow flex-shrink rounded-l-md focus:outline-none px-3"
           />
           <SearchIcon className="h-12 p-4" />
         </div>
 
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
           <div className="link">
-            <p>Hello Bibak Roy</p>
+            <p>Hello, Bibak Roy</p>
             <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
           <div className="link">
             <p>Returns</p>
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
-          <div className="flex items-center link relative">
+          <div
+            className="flex items-center link relative"
+            onClick={() => router.push("/checkout")}
+          >
             <span className="absolute w-4 h-4 bg-yellow-100 top-0 right-0 md:right-7 text-center rounded-full text-black font-bold">
               0
             </span>
