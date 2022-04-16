741c477dc2fe54a4a1e48541eebb99f2ddc81146
bibakroy <bibak68@student.sust.edu>
2022-04-16 22:02:51 +0600

    added firebase database

--- firebase.ts
+++ firebase.ts
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBKkkoZztFQXp7LeqhK-HJl4dUJTiiaBVo",
   authDomain: "e-vendor-347019.firebaseapp.com",
@@ -7,4 +9,27 @@ const firebaseConfig = {
   appId: "1:868478622401:web:5f8cc9286aae54622a1240",
 };
 
-export default firebaseConfig;
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+
+export default db;
+
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBKkkoZztFQXp7LeqhK-HJl4dUJTiiaBVo",
+//   authDomain: "e-vendor-347019.firebaseapp.com",
+//   projectId: "e-vendor-347019",
+//   storageBucket: "e-vendor-347019.appspot.com",
+//   messagingSenderId: "868478622401",
+//   appId: "1:868478622401:web:5f8cc9286aae54622a1240",
+// };
+
+// const app = initializeApp(firebaseConfig);
+
+// const db = getFirestore(app);
+// export default db;
--- pages/order.tsx
+++ pages/order.tsx
@@ -1,5 +1,95 @@
-const order = () => {
-  return <div>order</div>;
+import { getSession, useSession } from "next-auth/react";
+import Header from "../components/Header";
+import { GetServerSideProps } from "next";
+import db from "../firebase";
+import moment from "moment";
+// import { collection, getDocs } from "firebase/firestore";
+
+type OrderType = {
+  orders: {
+    id: string;
+    amount: number;
+    amountShipping: number;
+    image: string[];
+    timestamp: number;
+    items: Object[];
+  };
+};
+
+const order = ({ orders }: OrderType) => {
+  const { data: session } = useSession();
+
+  console.log({ orders });
+
+  return (
+    <div>
+      <Header />
+      <main className="max-w-screen-lg mx-auto p-10">
+        <h1 className="text-3xl border-b border-yellow-400 mb-2 pb-1">
+          Your Orders
+        </h1>
+        {session ? (
+          <h2>x orders</h2>
+        ) : (
+          <h2>Please sign in to see your orders</h2>
+        )}
+        <div className="mt-5 space-y-4"></div>
+      </main>
+    </div>
+  );
 };
 
 export default order;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+  //get the user's logged in credentials
+  const session = await getSession(context);
+  console.log({ session });
+
+  if (!session) {
+    return {
+      props: {},
+    };
+  }
+
+  const stripeOrders = await db
+    .collection("users")
+    .doc(session.user.email)
+    .collection("orders")
+    .orderBy("timestamp", "desc")
+    .get();
+
+  // const querySnapshot = await getDocs(collection(db, "users"));
+  // querySnapshot.forEach((doc) => {
+  //   console.log(`${doc.id} => ${doc.data()}`);
+  // });
+
+  // getDoc(session.user.email)
+  //   .collection("orders")
+  //   .orderBy("timestamp", "desc")
+  //   .get();
+
+  //stripe orders
+  const orders = await Promise.all(
+    stripeOrders.docs.map(async (order) => ({
+      id: order.id,
+      amount: order.data().amount,
+      amountShipping: order.data().amount_shipping,
+      image: order.data().images,
+      timestamp: moment(order.data().timestamp.toDate()).unix(),
+      items: (
+        await stripe.checkout.sessions.listLineItems(order.id, {
+          limit: 100,
+        })
+      ).data,
+    }))
+  );
+
+  return {
+    props: {
+      orders,
+    },
+  };
+};
