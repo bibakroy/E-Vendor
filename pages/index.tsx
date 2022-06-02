cd695a46a9f24121376cfc5c3b7bd0a98392d5bc
bibakroy <bibak68@student.sust.edu>
2022-06-03 02:24:38 +0600

    new Script

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,13 +22,13 @@ const Home = ({ products }: ProductsPropsType) => {
         id="my-script"
         dangerouslySetInnerHTML={{
           __html: `(function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:3001529,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`,
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3001555,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
         }}
       />
       <div className="bg-gray-100">
