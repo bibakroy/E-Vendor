17c3ae80b82940990482c6dfd352500949221e02
bibakroy <bibak68@student.sust.edu>
2022-06-03 02:04:26 +0600

    added hotjar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { getSession } from "next-auth/react";
 import Head from "next/head";
+import Script from "next/script";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 import { ProductFeed } from "../components/ProductFeed";
@@ -12,17 +13,31 @@ type ProductsPropsType = {
 
 const Home = ({ products }: ProductsPropsType) => {
   return (
-    <div className="bg-gray-100">
+    <>
       <Head>
         <title>E-Market</title>
         <link rel="icon" href="/logo-for-tab.svg" />
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3001529,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`,
+          }}
+        />
       </Head>
-      <Header />
-      <main className="max-w-screen-2xl mx-auto">
-        <Banner />
-        <ProductFeed products={products} />
-      </main>
-    </div>
+      <div className="bg-gray-100">
+        <Header />
+        <main className="max-w-screen-2xl mx-auto">
+          <Banner />
+          <ProductFeed products={products} />
+        </main>
+      </div>
+    </>
   );
 };
 
