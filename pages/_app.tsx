f9a50843bf11b623a94a88c05bdb602f8547adce
bibakroy <bibak68@student.sust.edu>
2022-06-03 21:01:10 +0600

    new script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         dangerouslySetInnerHTML={{
           __html: `(function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:3002785,hjsv:6};
+            h._hjSettings={hjid:3002851,hjsv:6};
             a=o.getElementsByTagName('head')[0];
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
