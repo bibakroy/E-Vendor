eab2085ed4d1bdab04842352851df9800306dd77
bibakroy <bibak68@student.sust.edu>
2022-06-03 20:36:20 +0600

    change script setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,13 +7,27 @@ import store from "../redux/store";
 import { Partytown } from "@builder.io/partytown/react";
 import Head from "next/head";
 import Hotjar from "../lib/third-party/hotjar";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <Partytown debug={true} forward={["dataLayer.push"]} />
-        <Hotjar />
+        <Script
+          id="hotjar"
+          type="text/partytown"
+          dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3002785,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+          }}
+        />
       </Head>
       <SessionProvider session={pageProps.session}>
         <Provider store={store}>
